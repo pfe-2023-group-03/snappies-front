b66e03a2ff594366726757657e2c19684e487d27
Laurent Vandermeersch <laurent.vandermeersch@student.vinci.be>
2023-12-04 16:51:47 +0100

    init front angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+// app.module.ts
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UsersModule } from './users/users.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, UsersModule, HttpClientModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
