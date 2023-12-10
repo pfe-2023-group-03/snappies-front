f47560af67c4a6d0dbfbf96706e68520c5c344f4
ThomasJohnen <thomas.johnen@hotmail.be>
2023-12-10 16:47:59 +0100

    setup: authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { LoginformComponent } from './loginform/loginform.component';
 import { AdminComponent } from './admin/admin.component';
+import { AuthenticationGuard } from './authentication-guard.guard';
 
 
 const routes: Routes = [
   { path: 'users', component: UsersComponent },
   { path: 'login', component: LoginformComponent},
-  { path: 'admin', component: AdminComponent}
+  { path: 'admin', component: AdminComponent, canActivate: [AuthenticationGuard] },
 ];
 
 @NgModule({
